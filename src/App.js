6b20d4875e518ab525d760af3f398391966ba14f
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-10-19 20:29:12 -0400

    Agregando funcionalidad para agregar una nueva tarea

--- src/App.js
+++ src/App.js
@@ -2,33 +2,43 @@ import React, { useEffect, useState } from "react";
 import Web3 from "web3";
 import { TODO_LIST_ABI, TODO_LIST_ADDRESS } from "./config";
 import "./App.css";
-import TodoList from './TodoList';
+import TodoList from "./TodoList";
 function App() {
   const [account, setAccount] = useState("");
   const [taskCount, setTaskCount] = useState(0);
   const [tasks, setTasks] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [todoList, setTodoList] = useState({});
   async function loadBlockChainData() {
     if (typeof window.ethereum !== "undefined") {
+      const array=[];
       const web3 = new Web3(Web3.givenProvider || "http://127.0.0.1:7645");
       // const network = await web3.eth.net.getNetworkType();
       const accounts = await web3.eth.getAccounts();
-      console.log(await web3.eth.getAccounts());
       setAccount(accounts[0]);
-      console.log(taskCount);
-      console.log(account);
       const todoList = new web3.eth.Contract(TODO_LIST_ABI, TODO_LIST_ADDRESS);
+      setTodoList(todoList);
       const taskC = await todoList.methods.taskCount().call();
       setTaskCount(taskC);
-      for (let i = 0; i <= taskC; i++) {
+      for (let i = 1; i <= taskC; i++) {
         const task = await todoList.methods.tasks(i).call();
-        setTasks([...tasks, task]);
+        array.push(task);
       }
+      setTasks(array);
       setLoading(false);
     } else {
       window.alert("No tienes instalado Metamask");
     }
   }
+  const createTask = async(content) => {
+    setLoading(true);
+    await todoList.methods
+      .createTask(content)
+      .send({ from: account })
+      .on("receipt", (receipt) => {
+        setLoading(false);
+      });
+  };
   useEffect(() => {
     loadBlockChainData();
   }, []);
@@ -57,7 +67,7 @@ function App() {
                 <p className="text-center">Loading...</p>
               </div>
             ) : (
-                <TodoList tasks={tasks}/>
+              <TodoList tasks={tasks} createTask={createTask}/>
             )}
           </main>
         </div>
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,37 +1,46 @@
-import React from "react";
+import React,{useState} from "react";
 // import Web3 from "web3";
 // import { TODO_LIST_ABI, TODO_LIST_ADDRESS } from "./config";
-function TodoList({tasks}) {
-  return (
-    <div id="content">
-      <form>
-        <input
-          id="newTask"
-          type="text"
-          className="form-control"
-          placeholder="Nueva tarea..."
-          required
-        />
-        <input
-          type="submit"
-          className="btn btn-primary btn-block mt-3"
-          hidden=""
-          value="Agregar tarea"
-        />
-      </form>
-      <ul id="taskList" className="list-unstyled">
-        {tasks.map((task) => (
-          <div className="taskTemplate" key={task.id}>
-            <label htmlFor="">
-              <input type="checkbox" name="" id="" />
-              <span className="ml-3 content">{task.content}</span>
-            </label>
-          </div>
-        ))}
-      </ul>
-      <ul id="completedTaskList" className="list-unstyled"></ul>
-    </div>
-  );
+function TodoList({tasks, createTask}) {
+    const [value,setValue] = useState('');
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        createTask(value);
+    }
+    const handleInputChange=(e)=>{
+        setValue(e.target.value);
+    }
+    return (
+        <div id="content">
+        <form onSubmit={handleSubmit}>
+            <input
+            id="newTask"
+            type="text"
+            className="form-control"
+            placeholder="Nueva tarea..."
+            required
+            onChange={handleInputChange}
+            />
+            <input
+            type="submit"
+            className="btn btn-primary btn-block mt-3"
+            hidden=""
+            value="Agregar tarea"
+            />
+        </form>
+        <ul id="taskList" className="list-unstyled">
+            {tasks.map((task) => (
+            <div className="taskTemplate" key={task.id}>
+                <label htmlFor="">
+                <input type="checkbox" name="" id="" />
+                <span className="ml-3 content">{task.content}</span>
+                </label>
+            </div>
+            ))}
+        </ul>
+        <ul id="completedTaskList" className="list-unstyled"></ul>
+        </div>
+    );
 }
 
 export default TodoList;
