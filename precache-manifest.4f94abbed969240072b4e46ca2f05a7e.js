58e012612179edd1e8bd74337d0e4bac540e4259
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-10-21 22:05:30 -0400

    Updates

---
+++ precache-manifest.4f94abbed969240072b4e46ca2f05a7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8002afc632d783122ab808c9b5f0da30",
+    "url": "/eth-todo-list-react/index.html"
+  },
+  {
+    "revision": "34b6c8df571f8de192b5",
+    "url": "/eth-todo-list-react/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "5251a4b566fab9967608",
+    "url": "/eth-todo-list-react/static/css/main.93b97114.chunk.css"
+  },
+  {
+    "revision": "34b6c8df571f8de192b5",
+    "url": "/eth-todo-list-react/static/js/2.b8b1288e.chunk.js"
+  },
+  {
+    "revision": "5c5a0857bb8747e9ae7db7917ecbbf79",
+    "url": "/eth-todo-list-react/static/js/2.b8b1288e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5251a4b566fab9967608",
+    "url": "/eth-todo-list-react/static/js/main.75f472f8.chunk.js"
+  },
+  {
+    "revision": "cd340af59254b9d2c48e",
+    "url": "/eth-todo-list-react/static/js/runtime-main.11e03e67.js"
+  }
+]);
